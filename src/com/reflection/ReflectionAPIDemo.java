0e88e4ec83fa987bbbbd0cb9b7e5d2a2e49be84f
mahavirsinhpw <mahavirsinhpuwar388@gmail.com>
2025-12-30 16:02:00 +0530

    solved spaced issue

--- src/com/reflection/ReflectionAPIDemo.java
+++ src/com/reflection/ReflectionAPIDemo.java
@@ -1,36 +1,37 @@
 package com.reflection;
 
+class A {
 
-class A{
-	
 }
 
-interface I1{
-	
+interface I1 {
+
 }
 
-interface I2{
-	
+interface I2 {
+
 }
-class Hello extends A implements I1, I2{
+
+class Hello extends A implements I1, I2 {
 	int a = 99;
 	int b;
-	
+
 	static {
 		System.out.println("sb in Hello");
 	}
 	{
 		System.out.println("IB in Hello");
 	}
+
 	Hello() {
 		System.out.println("Default Const in Hello");
 	}
-	
+
 	Hello(int b) {
 		System.out.println("1 para Const in Hello");
 		this.b = b;
 	}
-	
+
 	void show() {
 		System.out.println("show in HEllo");
 		System.out.println(a);
@@ -43,34 +44,33 @@ public class ReflectionAPIDemo {
 //		
 //		Hello h = new Hello();
 //		h.show();
-		
+
 		try {
 			Class cls = Class.forName("com.reflection.Hello");
 			Hello h = (Hello) cls.newInstance();
 			h.show();
-			
+
 			System.out.println("*****Information about class****\n");
-			
+
 			ClassLoader cl = cls.getClassLoader();
 			System.out.println(cl.getName());
 			System.out.println(cl);
-			
+
 			System.out.println("*********\n");
-			
+
 			System.out.println(cls); // internally toString is overridden in Class class
 			System.out.println(cls.getName());
 			System.out.println(cls.getPackageName());
 			System.out.println(cls.getSuperclass());
-			Class [] cls1 =cls.getInterfaces();
-			for(int i =0; i<cls1.length; i++) {
+			Class[] cls1 = cls.getInterfaces();
+			for (int i = 0; i < cls1.length; i++) {
 				System.out.println(cls1[i].getName());
 			}
-			
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+
 	}
 }
--- src/com/reflection/p2/ReflectionAPIDemo2.java
+++ src/com/reflection/p2/ReflectionAPIDemo2.java
@@ -4,10 +4,9 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 
 class A {
-	
+
 	int a = 101;
 
 	void mA() {
@@ -15,10 +14,8 @@ class A {
 	}
 }
 
-
 class B extends A {
 
-	
 	public static final int b = 201;
 	public int c = 303;
 	int d = 404;
@@ -38,8 +35,8 @@ class B extends A {
 
 	}
 
-	private void m2() throws Exception{
-		
+	private void m2() throws Exception {
+
 	}
 }
 
@@ -55,51 +52,49 @@ public class ReflectionAPIDemo2 {
 //			System.out.println(Modifier.FINAL);
 //			System.out.println(Modifier.PUBLIC);
 			System.out.println("*****\n");
-			Field [] f = cls.getFields();
-			for(int i =0; i<f.length; i++) {
-				System.out.println(f[i].getModifiers() +" Type : "+f[i].getType()+" Name :"+f[i].getName());
+			Field[] f = cls.getFields();
+			for (int i = 0; i < f.length; i++) {
+				System.out.println(f[i].getModifiers() + " Type : " + f[i].getType() + " Name :" + f[i].getName());
 			}
-			
+
 			System.out.println("*****\n");
-			Field [] fd = cls.getDeclaredFields();
-			for(int i =0; i<fd.length; i++) {
-				System.out.println(fd[i].getModifiers() +" Type : "+fd[i].getType()+" Name :"+fd[i].getName());
+			Field[] fd = cls.getDeclaredFields();
+			for (int i = 0; i < fd.length; i++) {
+				System.out.println(fd[i].getModifiers() + " Type : " + fd[i].getType() + " Name :" + fd[i].getName());
 			}
-			
+
 			System.out.println("*****\n");
-			Class [] c = cls.getClasses();
-			for(int i =0; i<c.length; i++) {
-				System.out.println(c[i].getModifiers() +" Type : "+c[i].getTypeName()+" Name :"+c[i].getName());
+			Class[] c = cls.getClasses();
+			for (int i = 0; i < c.length; i++) {
+				System.out.println(c[i].getModifiers() + " Type : " + c[i].getTypeName() + " Name :" + c[i].getName());
 			}
-			
+
 			System.out.println("*****\n");
-			Constructor [] con = cls.getConstructors();
-			for(int i =0; i<con.length; i++) {
-				System.out.println(con[i].getModifiers() +" Name :"+con[i].getName());
+			Constructor[] con = cls.getConstructors();
+			for (int i = 0; i < con.length; i++) {
+				System.out.println(con[i].getModifiers() + " Name :" + con[i].getName());
 			}
-			
+
 			System.out.println("*****\n");
-			Method [] m = cls.getMethods();
-			for(int i =0; i<m.length; i++) {
-				System.out.println(m[i].getModifiers() +" Type : "+m[i].getExceptionTypes()+" Name :"+m[i].getName());
+			Method[] m = cls.getMethods();
+			for (int i = 0; i < m.length; i++) {
+				System.out.println(
+						m[i].getModifiers() + " Type : " + m[i].getExceptionTypes() + " Name :" + m[i].getName());
 			}
-			
+
 			System.out.println("*****\n");
-			Method [] md = cls.getDeclaredMethods();
-			for(int i =0; i<md.length; i++) {
-				System.out.println(md[i].getModifiers() +" Type : "+md[i].getExceptionTypes()+" Name :"+md[i].getName());
+			Method[] md = cls.getDeclaredMethods();
+			for (int i = 0; i < md.length; i++) {
+				System.out.println(
+						md[i].getModifiers() + " Type : " + md[i].getExceptionTypes() + " Name :" + md[i].getName());
 			}
-			
+
 			System.out.println("*****\n");
-			Annotation [] a = cls.getAnnotations();
-			for(int i =0; i<a.length; i++) {
+			Annotation[] a = cls.getAnnotations();
+			for (int i = 0; i < a.length; i++) {
 				System.out.println(a[i].annotationType());
 			}
-			
-			
-			
-			
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
